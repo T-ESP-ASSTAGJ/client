fad7e07876034515f6a005f108f998a0c278e174
Antoine Duchesne <antduchesne@hotmail.fr>
2025-08-26 16:16:05 +0200

    :bug: test

--- apps/client/app/(tabs)/home/_components/post/post-footer.tsx
+++ apps/client/app/(tabs)/home/_components/post/post-footer.tsx
@@ -1,57 +1,49 @@
-import {IMusic, IStats} from "@/app/(tabs)/home/_types/post.types";
-import {Pressable, Text, View} from "react-native";
-import {Dot, LucideThumbsUp as ThumbUp, MessageCircle} from "lucide-react-native";
-import {getYearFromDate} from "@/helpers/format-date-helper";
+import type { IMusic, IStats } from "@/app/(tabs)/home/_types/post.types";
+import { getYearFromDate } from "@/helpers/format-date-helper";
+import {
+	Dot,
+	MessageCircle,
+	LucideThumbsUp as ThumbUp,
+} from "lucide-react-native";
+import { Pressable, Text, View } from "react-native";
 
 type PostFooterProps = {
-    music: IMusic,
-    stats: IStats
-}
+	music: IMusic;
+	stats: IStats;
+};
 
 export default function PostFooter({ music, stats }: PostFooterProps) {
-    return (
-        <Pressable className={"flex flex-col"}>
-            <View className="flex flex-row justify-between items-center pt-2.5 pl-1 pr-1">
-                <View className="flex flex-row items-center">
-                    <Text className="text-2xl font-semibold text-white items-center pb-1">
-                        {music.artist}
-                    </Text>
-                    <Dot
-                        className={"items-center"}
-                        color="white"
-                        size={30}
-                    />
-                    <Text className="font-medium text-white">
-                        {music.title}
-                    </Text>
-                </View>
+	return (
+		<Pressable className={"flex flex-col"}>
+			<View className="flex flex-row items-center justify-between pt-2.5 pr-1 pl-1">
+				<View className="flex flex-row items-center">
+					<Text className="items-center pb-1 font-semibold text-2xl text-white">
+						{music.artist}
+					</Text>
+					<Dot className={"items-center"} color="white" size={30} />
+					<Text className="font-medium text-white">{music.title}</Text>
+				</View>
 
-                <Text className="text-muted-foreground">
-                    {getYearFromDate(music.release_date)}
-                </Text>
-            </View>
-            <View>
-                <View className={"flex flex-row gap-2 items-center pl-1 pr-1 pt-3 pb-5"}>
-                    <View className={"flex flex-row gap-2 items-center"}>
-                        <ThumbUp
-                            color="#8D8D8D"
-                            size={14}
-                        />
-                        <Text className="text-muted-foreground text-sm">
-                            {stats.likes}
-                        </Text>
-                    </View>
-                    <View className={"flex flex-row gap-2 items-center"}>
-                        <MessageCircle
-                            color="#8D8D8D"
-                            size={14}
-                        />
-                        <Text className="text-muted-foreground text-sm">
-                            {stats.comments}
-                        </Text>
-                    </View>
-                </View>
-            </View>
-        </Pressable>
-    )
+				<Text className="text-muted-foreground">
+					{getYearFromDate(music.release_date)}
+				</Text>
+			</View>
+			<View>
+				<View
+					className={"flex flex-row items-center gap-2 pt-3 pr-1 pb-5 pl-1"}
+				>
+					<View className={"flex flex-row items-center gap-2"}>
+						<ThumbUp color="#8D8D8D" size={14} />
+						<Text className="text-muted-foreground text-sm">{stats.likes}</Text>
+					</View>
+					<View className={"flex flex-row items-center gap-2"}>
+						<MessageCircle color="#8D8D8D" size={14} />
+						<Text className="text-muted-foreground text-sm">
+							{stats.comments}
+						</Text>
+					</View>
+				</View>
+			</View>
+		</Pressable>
+	);
 }
--- apps/client/app/(tabs)/home/index.tsx
+++ apps/client/app/(tabs)/home/index.tsx
@@ -1,61 +1,61 @@
-import { MainView } from "@/components/ui/MainView";
-import {useCallback, useEffect, useState} from "react";
-import {Platform, RefreshControl, View} from "react-native";
-import { IUserPostsResponse, IPost } from "@/app/(tabs)/home/_types/post.types";
 import Post from "@/app/(tabs)/home/_components/post/post";
+import type {
+	IPost,
+	IUserPostsResponse,
+} from "@/app/(tabs)/home/_types/post.types";
+import { MainView } from "@/components/ui/MainView";
+import { HeaderAuth } from "@/components/ui/header/header-auth";
+import { mock_pagination, mock_posts } from "@/mock-data/post";
 import { FlashList } from "@shopify/flash-list";
-import {HeaderAuth} from "@/components/ui/header/header-auth";
-import {mock_pagination, mock_posts} from "@/mock-data/post";
+import { useCallback, useEffect, useState } from "react";
+import { Platform, RefreshControl, View } from "react-native";
 
 export default function HomePage() {
-    const [posts, setPosts] = useState<IUserPostsResponse>();
-    const [refreshing, setRefreshing] = useState(false);
+	const [posts, setPosts] = useState<IUserPostsResponse>();
+	const [refreshing, setRefreshing] = useState(false);
 
-    useEffect(() => {
-        setPosts({
-            posts: mock_posts,
-            pagination: mock_pagination
-        })
-    }, []);
+	useEffect(() => {
+		setPosts({
+			posts: mock_posts,
+			pagination: mock_pagination,
+		});
+	}, []);
 
-    const onRefresh = useCallback(() => {
-        setRefreshing(true);
-        console.log("🔄 refresh feed…");
-        setTimeout(() => {
-            setRefreshing(false);
-            console.log("✅ refresh terminé");
-        }, 1000);
-    }, []);
+	const onRefresh = useCallback(() => {
+		setRefreshing(true);
+		console.log("🔄 refresh feed…");
+		setTimeout(() => {
+			setRefreshing(false);
+			console.log("✅ refresh terminé");
+		}, 1000);
+	}, []);
 
 	return (
-		<MainView safeArea disableTouchableWrapper={true} >
-            <HeaderAuth searchIcon />
-            <View className={"w-full h-full"}>
-                <FlashList<IPost>
-                    data={posts?.posts ?? []}
-                    numColumns={1}
-                    renderItem={({ item } ) => (
-                        <View className="m-auto">
-                            <Post
-                                key={item.id}
-                                post={item}
-                            />
-                        </View>
-                    )}
-                    contentContainerStyle={{ paddingBottom: 180 }}
-                    estimatedItemSize={60}
-                    refreshControl={
-                        <RefreshControl
-                            onRefresh={onRefresh}
-                            refreshing={refreshing}
-                            tintColor="#ffffff"
-                            colors={["#ffffff"]}
-                            progressBackgroundColor="#ffffff"
-                            progressViewOffset={Platform.select({ ios: 24, android: 16 })}
-                        />
-                    }
-                />
-            </View>
+		<MainView safeArea disableTouchableWrapper={true}>
+			<HeaderAuth searchIcon />
+			<View className={"h-full w-full"}>
+				<FlashList<IPost>
+					data={posts?.posts ?? []}
+					numColumns={1}
+					renderItem={({ item }) => (
+						<View className="m-auto">
+							<Post key={item.id} post={item} />
+						</View>
+					)}
+					contentContainerStyle={{ paddingBottom: 180 }}
+					estimatedItemSize={60}
+					refreshControl={
+						<RefreshControl
+							onRefresh={onRefresh}
+							refreshing={refreshing}
+							tintColor="#ffffff"
+							colors={["#ffffff"]}
+							progressBackgroundColor="#ffffff"
+							progressViewOffset={Platform.select({ ios: 24, android: 16 })}
+						/>
+					}
+				/>
+			</View>
 		</MainView>
 	);
 }
